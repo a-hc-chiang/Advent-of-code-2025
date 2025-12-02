03bb88bb2f82aa34650a5ec5893e6f58ffcd8e3c
a-hc-chiang <a.hc.chiang05@gmail.com>
2025-12-01 22:58:49 -0800

    Day 1 part 2 was a flop lol started thinking about day 2 part 1 tho

---
+++ solutions/day-1/password-cracker2.py
@@ -0,0 +1,33 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent.parent))
+from parser import get_input
+
+example_case = ['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82']
+input = get_input("day-1.txt")
+
+# range = [0, 99]
+start = 50
+MIN = 0
+MAX = 99
+res = 0
+
+for turn in input:
+  direction = turn[0]
+  num = int(turn[1:])
+  res += abs(num) // 100
+  value = num % 100
+  print(num, start, res)
+  if direction == "L":
+    movement = -value
+  else:
+    movement = value
+  end = start + movement
+  if direction == "L":
+    if end < MIN:
+      res += 1
+  else:
+    if end > MAX:
+      res += 1
+  start = end % 100
+print(res) # WRONG OUTPUT FOR SOME GODDAMN REASON???
