8ae9b11a340e3dc2864bcc1487f5e21e02b8df3d
a-hc-chiang <a.hc.chiang05@gmail.com>
2025-12-02 22:13:06 -0800

    Completed day 2 with working solutions for both parts

--- solutions/day-2/id-checker2.py
+++ solutions/day-2/id-checker2.py
@@ -1,24 +1,18 @@
+import math
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent.parent))
 from parser import get_input
-import math
 
 example_case = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 input = get_input("day-2.txt", ",")
 
-'''
-IDs have no leading 0s 
-find invalid IDs where: 
-
-an ID has a sequence of when a digit is repeated twice
-we can skip on anything that does not have an even placement 
-if x log 10 is even then we can skip evaluation 
-except im lazy im going to just convert this to a string and count the length LOL
-'''
-
 input_as_list = example_case.split(",")
 res = set()
+
+def split_into_parts(s: str, n: int) -> list[str]:
+  part_len = len(s) // n
+  return [s[i:i + part_len] for i in range(0, len(s), part_len)]
 
 for ids in input:
   start_and_end = ids.split("-")
@@ -29,10 +23,19 @@ for ids in input:
   end_num = int(end)
   for i in range(start_num, end_num + 1):
     as_str = str(i)
-    if len(as_str) % 2 != 0:
-      continue
-    first_half = as_str[:int(len(as_str)/2)]
-    second_half = as_str[int(len(as_str)/2):]
-    if first_half == second_half:
-      res.add(i)
-print(sum(res)) #YIPPEE ACCEPTED!
+    first_idx = 0
+    last_idx = 0
+    length = len(as_str)
+    # print(length, start, end, res)
+    for j in range(1, length):
+      if length % j != 0:
+        continue
+      mult = int(length / j)
+      splitted = split_into_parts(as_str, mult)
+      # print(f"splitted: {splitted}")
+      duplicates = set(splitted)
+      if len(duplicates) == 1:
+        res.add(int(as_str))
+        break
+# print(res)
+print(sum(res))
