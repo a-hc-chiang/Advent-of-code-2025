d0ca8111933e2a1fb8cb0dd6bd7e2a7d8c6106bc
a-hc-chiang <a.hc.chiang05@gmail.com>
2025-12-18 15:05:06 -0800

    Day 7 part 2 completed

--- solutions/day-7/festive-tachyon-beam2.py
+++ solutions/day-7/festive-tachyon-beam2.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent.parent))
 from parser import get_input
+from collections import defaultdict
 
 example_case = """.......S.......
 ...............
@@ -27,45 +28,29 @@ def find_s_idx(s):
   return -1
 
 example_case = example_case.split("\n")
-# input = get_input("day-7.txt", "\n")
+input = get_input("day-7.txt", "\n")
 res = 0
 
-s_idx = find_s_idx(example_case[0])
-beam_idx = [[s_idx]]
+s_idx = find_s_idx(input[0])
+curr = {s_idx:1}
 
-for line in example_case[1:]:
+for line in input[1:]:
   if "^" not in line:
     continue
-
-  to_add = set()
-  for i in beam_idx[-1]:
-    # print("to_add", to_add)
-    flag1, flag2 = False, False
+  to_add = defaultdict(int)
+  for i in curr:
     if line[i] == "^":
       if line[i-1] == ".":
-        if line[i-1] not in to_add:
-          to_add.add(i-1)
-          res += 1
-        flag1 = True
+        to_add[i-1] += curr[i]
       if line[i+1] == ".":
-        if line[i+1] not in to_add:
-          to_add.add(i+1)
-          res += 1
-        flag2 = True
+        to_add[i+1] += curr[i]
     else:
-      to_add.add(i)
-    # if flag1 and flag2: 
-    #   res += 2
-    # if flag1 or flag2:
-    #   res += 1
-  if list(to_add) not in beam_idx:
-    beam_idx.append(list(to_add))
+      to_add[i] += curr[i]
+  curr = to_add
 
-print(beam_idx)
-res2 = 0
+# print(beam_idx)
+for k in curr: 
+  res += curr[k]
 
-for beam in beam_idx:
-  res2 += len(beam)
-print(res)
-print(res2)
-# print(sum(res))
+print(res) # YIPPEE ACCEPTED!
+# print(curr)
