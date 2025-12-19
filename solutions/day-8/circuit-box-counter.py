190948991a85c76f3f086d264a278d8331be1c94
a-hc-chiang <a.hc.chiang05@gmail.com>
2025-12-18 17:43:51 -0800

    Barely completed day 8 part 1 LOL

---
+++ solutions/day-8/circuit-box-counter.py
@@ -0,0 +1,121 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent.parent))
+from parser import get_input
+import heapq
+import math
+
+example_case = """162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689"""
+
+def find_distance(a, b):
+  return math.sqrt(sum((b[i] - a[i])**2 for i in range(3)))
+
+# constructing adjacency list
+#turning input into tuples
+# example_case = example_case.split("\n")
+input = get_input("day-8.txt", "\n")
+
+nodes = {}
+for i in range(len(input)):
+  as_list = input[i].split(",")
+  # print(as_list)
+  node = (int(as_list[0]), int(as_list[1]), int(as_list[2]))
+  nodes[i] = node
+
+# print(nodes)
+adj_list = []
+# distances to node 
+for i in range(len(nodes)-1): 
+  for j in range(i+1, len(nodes)): 
+    distance = find_distance(nodes[i], nodes[j])
+    adj_list.append((distance, i, j))
+# print(adj_list)
+
+heapq.heapify(adj_list)
+groups = []
+node_to_group = {}
+visited = set()
+
+def add_new_group(a, b):
+  group = {a, b}
+  idx = len(groups)
+  groups.append(group)
+  node_to_group[a] = idx
+  node_to_group[b] = idx
+
+def add_to_group(node, group_idx):
+  groups[group_idx].add(node)
+  node_to_group[node] = group_idx
+
+def merge_groups(idx1, idx2):
+  if idx1 == idx2:
+    return
+
+  # Merge smaller into larger (optional optimization)
+  if len(groups[idx1]) < len(groups[idx2]):
+    idx1, idx2 = idx2, idx1
+
+  for node in groups[idx2]:
+    groups[idx1].add(node)
+    node_to_group[node] = idx1
+
+  groups[idx2].clear()
+
+
+# ChatGPT 5.2 Assisted in creating the loop conditions + additional helper functions:
+i = 0
+while adj_list:
+  if i == 999:
+    break
+  _, a, b = heapq.heappop(adj_list)
+  visited.update([a, b])
+  g1 = node_to_group.get(a)
+  g2 = node_to_group.get(b)
+  
+  if g1 is not None and g2 is not None:
+    if g1 == g2:
+      i += 1
+      continue
+    merge_groups(g1, g2)
+
+  elif g1 is not None:
+    add_to_group(b, g1)
+
+  elif g2 is not None:
+    add_to_group(a, g2)
+
+  else:
+    add_new_group(a, b)
+  # print(a, b, g1, g2, groups)
+  i += 1
+
+groups = [g for g in groups if g]
+lengths = []
+
+for group in groups:
+  lengths.append(-len(group))
+
+heapq.heapify(lengths)
+print(lengths)
+product = heapq.heappop(lengths) * heapq.heappop(lengths) * heapq.heappop(lengths)
+print(groups)
+print(abs(product)) # WAAAAH ACCEPTED 
